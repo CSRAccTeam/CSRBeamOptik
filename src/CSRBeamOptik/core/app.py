175523638825530e4156fc292376303f75521b44
Cristopher Cortes <ecortesg95@gmail.com>
2019-11-07 16:52:02 +0100

    Move imports to the top

--- src/CSRBeamOptik/core/app.py
+++ src/CSRBeamOptik/core/app.py
@@ -1,19 +1,23 @@
 import sys
+import requests
+
 from PyQt5.QtCore import QTimer, QCoreApplication
+from PyQt5.QtWidgets import QApplication
+
+from CSRBeamOptik.beamOptik.IonBeam import ChargedParticle
+from CSRBeamOptik.util.loadFiles import readYamlFile
+
 
 def initApp(particle, EUNetManager, argv=None, gui=True):
     from CSRBeamOptik.widgets.mainWindow import mainWindow
-    from PyQt5.QtWidgets import QApplication
+    
     if argv is None: argv = sys.argv
     if gui:
-        from PyQt5.QtWidgets import QApplication
-        from importlib_resources import read_text
         app   = QApplication(argv)
         mainW = mainWindow(particle, EUNetManager)
         sys.exit(app.exec_())
     
 def getProton():
-    from CSRBeamOptik.beamOptik.IonBeam import ChargedParticle
     eKin = 300.
     Q    = 1.
     mass = 1.
@@ -25,17 +29,16 @@ def _readIonData(ionData):
     ionData = {}
     for line in lines:
         data = line.split('=')
-        if (len(data) == 2.):
-            ionData.update({data[0] : float(data[1])})
+        if (len(data) == 2.): ionData.update({data[0] : float(data[1])})
     return ionData
 
 def initIonBeam():
-    from CSRBeamOptik.beamOptik.IonBeam import ChargedParticle
-    from CSRBeamOptik.util.loadFiles import readYamlFile
-    import requests
-
-    ionConfig = readYamlFile('/home/blm/ccortes/MPIK/CSR/CSRBeamOptik' + \
-                             '/configFiles/ionDict.yaml')
+    """
+    Loads the data from the web server. Note that the ion 
+    might not match with the ongoing experiment.
+    """
+    ionConfig   = readYamlFile('/home/blm/ccortes/MPIK/CSR/CSRBeamOptik' + \
+                               '/configFiles/ionDict.yaml')
     ionDataURL  = ionConfig['ionDataURL']
     ionDataKeys = ionConfig['ionDataKeys']
     ionData = requests.get(ionDataURL)
@@ -47,6 +50,8 @@ def initIonBeam():
     Q    = ionData['LadungIon']
     mass = ionData['MasseIon']
     ion  = ChargedParticle(eKin, Q, mass)
+
+    #TODO: Implement user input definition of beam 
      
     return ion
 
